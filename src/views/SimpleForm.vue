246a82805dc8a211e3b43c417bd778d4aba1c23d
Raiko Murulauk <raiko4238@gmail.com>
2021-04-14 19:22:00 +0300

    Submitting

--- src/views/SimpleForm.vue
+++ src/views/SimpleForm.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Create an event</h1>
-    <form>
+    <form @submit.prevent="sendForm">
 
       <BaseSelect
         v-model="event.category"
@@ -61,6 +61,7 @@
 </template>
 
 <script>
+import axios from 'axios'
 import BaseCheckbox from '../components/BaseCheckbox.vue'
 export default {
   components: { BaseCheckbox },
@@ -90,6 +91,19 @@ export default {
         { label: 'Yes', value: 1 },
         { label: 'No', value: 0 }
       ]
+    }
+  },
+  methods: {
+    sendForm () {
+      axios.post(
+        'https://my-json-server.typicode.com/Code-Pop/Vue-3-Forms/events', this.event
+      )
+        .then(function (response) {
+          console.log('Response', response)
+        })
+        .catch(function (err) {
+          console.log('Error', err)
+        })
     }
   }
 }
